583b8c463b6db85bd97c86620353554b46d706ca
Jonah Brown <jswish02@gmail.com>
2019-09-19 20:52:39 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -9,7 +9,8 @@ def add_student(student_name, grade)
   roster[grade] << student_name
 end
 def grade(student_grade)
-  
+  roster(student_grade)
+end
 def sort
   sorted = {}
   roster.each do |student name, roster|
